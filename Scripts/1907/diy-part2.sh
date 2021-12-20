6de051395fbea7d3a4bafca1de9985e5d7d4932e
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2021-12-20 09:21:43 +0800

    Update diy-part2.sh

--- Scripts/1907/diy-part2.sh
+++ Scripts/1907/diy-part2.sh
@@ -13,6 +13,11 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/10.1.10.1/g' package/base-files/files/bin/config_generate
 
+#内核版本是会随着源码更新而改变的，在Lienol/openwrt的源码查看最好，以X86机型为例，源码的target/linux/x86文件夹可以看到有几个内核版本，x86文件夹里Makefile就可以查看源码正在使用什么内核
+#修改版本内核（19.07默认使用4.14内核，还有4.19跟4.9的内核，自行选择。这个跟LEDE的有点不一样，这个是修改一行代码的）
+#sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile  #修改内核版本
+
+
 # Add Lienol's Packages
 git clone --depth=1 https://github.com/Lienol/openwrt-package
 rm -rf ../lean/luci-app-kodexplorer
