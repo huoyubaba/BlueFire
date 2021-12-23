0fa345621fac483a2ac1c261be71fdac30f2c785
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2021-12-23 20:38:22 +0800

    Update diy-part1.sh

--- Scripts/1907/diy-part1.sh
+++ Scripts/1907/diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lede https://github.com/coolsnowwolf/lede/tree/master/package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
