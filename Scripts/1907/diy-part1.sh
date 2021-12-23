afe15556ff1c9a8baa1ca5dea9f4e2289281b3c4
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2021-12-23 19:31:43 +0800

    Update diy-part1.sh

--- Scripts/1907/diy-part1.sh
+++ Scripts/1907/diy-part1.sh
@@ -17,12 +17,6 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git small https://github.com/Thanf-code/small-package' feeds.conf.default
-#svn co https://github.com/Thanf-code/OpenAppFilter package/OpenAppFilter > /dev/null 2>&1
-#svn co https://github.com/Thanf-code/iptvhelper package/iptvhelper > /dev/null 2>&1
-#git clone https://github.com/Hyy2001X/luci-app-autoupdate.git package/luci-app-autoupdate
-#git clone https://github.com/Thanf-code/luci-app-control-weburl.git package/luci-app-control-weburl
-#git clone https://github.com/Thanf-code/luci-app-omcproxy.git package/luci-app-omcproxy
 git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 git clone https://github.com/Thanf-code/luci-app-cpufreq.git package/luci-app-cpufreq
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns  #smartdns DNS加速
