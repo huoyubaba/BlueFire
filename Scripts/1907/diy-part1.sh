b51ada5b0aacb52ad6d7fcb1f919aa55ffc89025
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2021-12-30 13:59:11 +0800

    Update diy-part1.sh

--- Scripts/1907/diy-part1.sh
+++ Scripts/1907/diy-part1.sh
@@ -20,3 +20,4 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 git clone https://github.com/Thanf-code/luci-app-cpufreq.git package/luci-app-cpufreq
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns  #smartdns DNS加速
+git clone https://github.com/iamaluckyguy/luci-app-smartinfo.git package/luci-app-smartinfo
