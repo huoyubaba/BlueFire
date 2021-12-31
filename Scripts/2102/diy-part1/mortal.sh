b56aa7cc50b66690771ff26414ce1da8b97a92aa
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2021-12-31 17:13:50 +0800

    Update mortal.sh

--- Scripts/2102/diy-part1/mortal.sh
+++ Scripts/2102/diy-part1/mortal.sh
@@ -8,19 +8,10 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# fw876/helloworld
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git small https://github.com/Thanf-code/small-package' feeds.conf.default
-#svn co https://github.com/Thanf-code/OpenAppFilter package/OpenAppFilter > /dev/null 2>&1
-#svn co https://github.com/Thanf-code/iptvhelper package/iptvhelper > /dev/null 2>&1
-#git clone https://github.com/Hyy2001X/luci-app-autoupdate.git package/luci-app-autoupdate
-#git clone https://github.com/Thanf-code/luci-app-control-weburl.git package/luci-app-control-weburl
+
+sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
 #git clone https://github.com/Thanf-code/luci-app-omcproxy.git package/luci-app-omcproxy
 git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 git clone https://github.com/Thanf-code/luci-app-cpufreq.git package/luci-app-cpufreq
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns  #smartdns DNS加速
-git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-autotimeset
 git clone https://github.com/animefansxj/luci-app-smartinfo.git package/luci-app-smartinfo
