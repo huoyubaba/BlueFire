b6594f61996788dec19775ee2b31167423dcb48b
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2022-02-22 14:38:23 +0800

    Update diy-part2.sh

--- Scripts/diy-part2.sh
+++ Scripts/diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.1.10.1/g' package/base-files/files/bin/config_generate
+
+# Modify hostname
+sed -i 's/OpenWrt/BlueFire/g' package/base-files/files/bin/config_generate
