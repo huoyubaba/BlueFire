bcd744a77bbbc780c58bd80813a3d45d0bce987d
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2022-03-30 19:33:45 +0800

    Update diy-part3.sh

--- Scripts/diy-part3.sh
+++ Scripts/diy-part3.sh
@@ -22,13 +22,13 @@
 # git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns  #smartdns DNS加速
 # git clone https://github.com/iamaluckyguy/luci-app-smartinfo.git package/luci-app-smartinfo
 git clone https://github.com/erdoukki/luci-app-arpbind.git package/luci-app-arpbind
-git clone https://github.com/zlg98/luci-app-vlmcsd.git package/luci-app-vlmcsd
+# git clone https://github.com/zlg98/luci-app-vlmcsd.git package/luci-app-vlmcsd
 git clone https://github.com/loryncien/luci-app-sqm.git package/luci-app-sqm
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-smartdns package/luci-app-smartdns
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-autoreboot package/luci-app-autoreboot
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-ramfree package/luci-app-ramfree
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-usb3disable package/luci-app-usb3disable
 svn co https://github.com/kenzok8/jell/trunk/luci-app-accesscontrol package/luci-app-accesscontrol
-svn co https://github.com/kenzok8/jell/trunk/luci-app-samba4 package/luci-app-samba4
+# svn co https://github.com/kenzok8/jell/trunk/luci-app-samba4 package/luci-app-samba4
 svn co https://github.com/kenzok8/jell/trunk/luci-app-usb-printer package/luci-app-usb-printer
-svn co https://github.com/kenzok8/jell/trunk/luci-app-vsftpd package/luci-app-vsftpd
+# svn co https://github.com/kenzok8/jell/trunk/luci-app-vsftpd package/luci-app-vsftpd
