6a301076c8ec15d153e1e230a79becc18e6da776
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2022-03-30 15:43:37 +0800

    Update diy-part3.sh

--- Scripts/diy-part3.sh
+++ Scripts/diy-part3.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 # sed -i '$a src-git lede https://github.com/coolsnowwolf/lede/tree/master/package' feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/jell' feeds.conf.default   #19.07 18.06 软件源
-sed -i '$a src-git kenzo https://github.com/kenzok8/small-package' feeds.conf.default   # 软件源
+# sed -i '$a src-git kenzo https://github.com/kenzok8/small-package' feeds.conf.default   # 软件源
 # git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 # git clone https://github.com/Thanf-code/luci-app-cpufreq.git package/luci-app-cpufreq
 # git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns  #smartdns DNS加速
@@ -24,3 +24,4 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/small-package' feeds.conf.de
 git clone https://github.com/erdoukki/luci-app-arpbind.git package/luci-app-arpbind
 git clone https://github.com/zlg98/luci-app-vlmcsd.git package/luci-app-vlmcsd
 git clone https://github.com/loryncien/luci-app-sqm.git package/luci-app-sqm
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/simple-obfs package/simple-obfs
