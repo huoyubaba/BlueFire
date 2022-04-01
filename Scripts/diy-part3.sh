e2b0498b90e85ee19f14dea10117203b5aaaeef0
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2022-04-02 07:56:33 +0800

    Update diy-part3.sh

--- Scripts/diy-part3.sh
+++ Scripts/diy-part3.sh
@@ -24,7 +24,7 @@
 git clone https://github.com/erdoukki/luci-app-arpbind.git package/luci-app-arpbind
 # git clone https://github.com/zlg98/luci-app-vlmcsd.git package/luci-app-vlmcsd
 git clone https://github.com/loryncien/luci-app-sqm.git package/luci-app-sqm
-git clone https://github.com/kongfl888/luci-i18n-sqm-zh-cn.git package/luci-i18n-sqm-zh-cn
+# git clone https://github.com/kongfl888/luci-i18n-sqm-zh-cn.git package/luci-i18n-sqm-zh-cn
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-smartdns package/luci-app-smartdns
 svn co https://github.com/kenzok8/small-package/trunk/smartdns package/smartdns
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-autoreboot package/luci-app-autoreboot
