4f9e5350393429f11fe12011871c7517f2906ca3
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2022-05-21 11:41:03 +0800

    Update diy-part3.sh

--- Scripts/diy-part3.sh
+++ Scripts/diy-part3.sh
@@ -38,7 +38,7 @@ svn co https://github.com/kenzok8/small-package/trunk/luci-app-oaf package/luci-
 svn co https://github.com/kenzok8/small-package/trunk/open-app-filter package/open-app-filter
 svn co https://github.com/kenzok8/small-package/trunk/oaf package/oaf
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-nfs package/luci-app-nfs
-
+svn co https://github.com/kenzok8/small-package/trunk/luci-app-easymesh package/luci-app-easymesh
 git clone https://github.com/erdoukki/luci-app-arpbind.git package/luci-app-arpbind
 
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-autoreboot package/luci-app-autoreboot
