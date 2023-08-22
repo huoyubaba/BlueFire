ac470d74ec9cc62862356f7bd3a3f2debe4a6c2d
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2023-08-22 09:57:34 +0800

    Update diy-part1.sh

--- Scripts/diy-part1.sh
+++ Scripts/diy-part1.sh
@@ -14,8 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# sed -i '$a src-git lede https://github.com/coolsnowwolf/lede/tree/master/package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/small-package ' feeds.conf.default   #19.07 18.06 软件源
+sed -i '$a src-git lede https://github.com/coolsnowwolf/luci/applications' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/small-package ' feeds.conf.default   #19.07 18.06 软件源
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default   # 软件源
 # git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 # git clone https://github.com/Thanf-code/luci-app-cpufreq.git package/luci-app-cpufreq
@@ -24,6 +24,6 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/small-package ' feeds.conf.d
 # git clone https://github.com/iamaluckyguy/luci-app-smartinfo.git package/luci-app-smartinfo
 # git clone https://github.com/erdoukki/luci-app-arpbind.git package/luci-app-arpbind
 # git clone https://github.com/xsmzhzy/luci-app-vlmcsd.git package/luci-app-vlmcsd
-git clone https://github.com/loryncien/luci-app-sqm.git package/luci-app-sqm
+# git clone https://github.com/loryncien/luci-app-sqm.git package/luci-app-sqm
 # git clone https://github.com/fshh1988/luci-app-nfs.git package/luci-app-nfs
 # git clone https://github.com/deplives/luci-app-samba.git package/luci-app-samba
